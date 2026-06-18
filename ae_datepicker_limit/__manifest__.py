5c8ea4e5917c35f77139bd76c2bfe24d71c56ddd
Aska Erlangga <ae.askaerlangga@gmail.com>
2026-06-18 10:38:41 +0700

    [IMP] ae_datepicker_limit: update license from AGPL-3 to LGPL-3

--- ae_datepicker_limit/__manifest__.py
+++ ae_datepicker_limit/__manifest__.py
@@ -6,7 +6,7 @@
     "website": "https://www.askaerlangga.my.id",
     "category": "Technical",
     "depends": ["web"],
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "assets": {
         "web.assets_backend": [
             "ae_datepicker_limit/static/src/js/datetime_field_limit.js",
